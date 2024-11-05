c24e81d185b8ccd4eba0cb6363f43cbe973a8f0a
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-11-05 21:12:23 +0200

    build transaction page for mobile

--- app/pages/transactions/page.tsx
+++ app/pages/transactions/page.tsx
@@ -1,14 +1,52 @@
-import data from "../../../data.json"
+import Transaction from "@/app/components/Transaction";
+import data from "../../../data.json";
+import Image from "next/image";
 
-console.log(data.transactions)
 const Transactions = () => {
   return (
     <div className="pages-padding md:p-9">
       <h3 className="text-[2em] font-bold">Transactions</h3>
       <div className="mt-5 card-style">
+        <div className="flex-row">
+          <div className="flex-row border border-grey500 w-[320px] p-3 rounded-lg">
+            <input
+              type="text"
+              placeholder="Search transactions"
+              className="text-[.9em] outline-none"
+            />
+            <Image
+              src={"/assets/images/icon-search.svg"}
+              alt="magnifying glass"
+              width={15}
+              height={15}
+            />
+          </div>
+
+          <div className="flex items-center gap-6">
+            <Image
+              src={"/assets/images/icon-sort-mobile.svg"}
+              alt="ellipsis icon"
+              width={18}
+              height={18}
+              className="cursor-pointer"
+            />
+            <Image
+              src={"/assets/images/icon-filter-mobile.svg"}
+              alt="filter icon"
+              width={18}
+              height={18}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
+
         {data.transactions.map((transaction, i) => (
           <div key={i}>
-            <h1>{transaction.name}</h1>
+            <Transaction
+              transaction={transaction}
+              index={i}
+              arrLength={data.transactions.length}
+            />
           </div>
         ))}
       </div>
