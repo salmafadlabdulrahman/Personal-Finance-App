2e5db995af7850cf42236e82a552536d3cfabc98
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-11-19 20:12:33 +0200

    add margins to the expenses

--- app/components/Expense.tsx
+++ app/components/Expense.tsx
@@ -22,9 +22,9 @@ const Expense = ({ expenses, styles, budgetPage }: ExpenseProps) => {
       {expenses.map((item, i) => (
         <div
           key={i}
-          className={`flex items-center gap-4 w-[50%] md:w-[100%] mb-4 md:px-2 md:p-0 ${
-            budgetPage && "w-full"
-          }`}
+          className={`flex items-center gap-4 w-[50%] md:w-[100%] mb-5 pb-5 ${
+            i !== expenses.length - 1 ? "border-b-[1px] border-gray-200" : ""
+          } ${budgetPage && "w-full"}`}
         >
           <span
             className={`block ${
@@ -32,9 +32,9 @@ const Expense = ({ expenses, styles, budgetPage }: ExpenseProps) => {
             } w-[4px] rounded-full`}
             style={{ backgroundColor: item.theme }}
           ></span>
-          <div className={`${budgetPage ? "flex-row w-full" : ""} `}>
+          <div className={`${budgetPage ? "flex-row w-full" : ""}  `}>
             {" "}
-            <p className="text-grey500 text-[.8em]">{item.category}</p>
+            <p className="text-grey500 text-[.9em]">{item.category}</p>
             {budgetPage ? (
               <div className="flex items-center gap-2">
                 <p className="text-grey900 font-bold text-[.9em]">
